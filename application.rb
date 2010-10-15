7d6f08551c26d504260c6361e1f4c638f2d76a4a
Aaron Moodie <aaron@aaron.local>
2010-10-15 15:13:52 +1100

    moved set and use methods to application.rb

--- application.rb
+++ application.rb
@@ -17,6 +17,15 @@ require './helpers.rb'
 
 
 
+use Rack::Session::Cookie
+use Rack::Flash
+set :session_fail, '/login'
+set :session_secret, '33tcak3&d13!'
+set :views, File.dirname(__FILE__) + '/views'
+set :public, File.dirname(__FILE__) + '/public'
+
+
+
 # -------- URL ROUTING -------- #
 
 
